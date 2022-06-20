4892eda0b17f062af679357dc9bafbc95e990951
Demi <nosedademian@gmail.com>
2022-06-20 17:30:16 -0300

    fix: :bug: TopicDropdown Bug Fixed

--- src/components/TopicDropdown.js
+++ src/components/TopicDropdown.js
@@ -3,13 +3,18 @@ import { useOutsideAlerter } from "../hooks/outsideAlerter";
 import angularLogo from "../assets/imgs/angular_icon.png";
 import reactLogo from "../assets/imgs/react_icon.png";
 import vuejsLogo from "../assets/imgs/vuejs_icon.png";
+import { useState } from "react";
 const style_query = {
   background: "#F7F7F7",
 };
 
 const TopicDropdown = ({ query, setQuery }) => {
   const { visible, setVisible, ref } = useOutsideAlerter(false);
-
+  const [options, setOptions] = useState([
+    { id: 1, text: "Angular", imgSrc: angularLogo },
+    { id: 2, text: "Reactjs", imgSrc: reactLogo },
+    { id: 3, text: "Vuejs", imgSrc: vuejsLogo },
+  ]);
   const handleOpenDropdown = () => {
     setVisible((prevState) => !prevState);
   };
@@ -19,12 +24,6 @@ const TopicDropdown = ({ query, setQuery }) => {
     setQuery(e.target.text);
     handleOpenDropdown();
   };
-
-  const options = [
-    { id: 1, text: "Angular", imgSrc: angularLogo },
-    { id: 2, text: "Reactjs", imgSrc: reactLogo },
-    { id: 3, text: "Vuejs", imgSrc: vuejsLogo },
-  ];
 
   const selectedInfo = options.filter((option) => option.text === query);
 
@@ -42,7 +41,7 @@ const TopicDropdown = ({ query, setQuery }) => {
       >
         <p>
           {selectedInfo.length > 0 ? (
-            <img src={selectedInfo[0].imgSrc} alt={selectedInfo[0].text} />
+            <img src={selectedInfo[0].imgSrc} alt="Selected img" />
           ) : null}
 
           {query}
@@ -59,7 +58,7 @@ const TopicDropdown = ({ query, setQuery }) => {
               className="link text_bg"
               key={option.id}
             >
-              <img src={option.imgSrc} alt={selectedInfo[0].text} />
+              <img src={option.imgSrc} alt={option.text} />
               {option.text}
             </a>
           ))}
